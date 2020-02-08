453f612d4b0906de30d067dc60eeb88731fc88ce
HusmithaSilva <husmitha.silva0@gmail.com>
2020-02-08 21:50:19 +0530

    COMMIT

---
+++ src/Intermediate1/clickCounter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class clickCounter extends Component{
+    render(){
+        return(
+            <button>
+                click x times
+            </button>
+        )
+    }
+}
+export default clickCounter
